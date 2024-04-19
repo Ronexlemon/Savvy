6316dbddce6e183245d47fa3418d2a7796d68fee
RonexLemon <ronexondimu@gmail.com>
2024-04-19 07:44:34 +0300

    statistics

--- client/savvyy/packages/react-app/components/pages/Statistics.tsx
+++ client/savvyy/packages/react-app/components/pages/Statistics.tsx
@@ -13,20 +13,22 @@ import { BsFillSendFill } from "react-icons/bs";
 import { RiBankFill } from "react-icons/ri";
 import { FaCircleCheck } from "react-icons/fa6";
 import { SiCashapp } from "react-icons/si";
+
+import { FaDownload } from "react-icons/fa6";
 export default function StatisticPage() {
   return (
     <div className="flex h-full w-screen bg-gray-100 relative">
       <Flex h="100vh" direction="column" maxH="90vh" w="100%">
       
-        <Box w="100%" bgGradient="linear(to-t, green.500, #2F7E79)" height="20%" roundedBottom="20px" >
-        <Flex direction='row' w='100%' height='50%'alignItems='center'  padding={8} justifyContent='space-between'>
+        <Box w="100%"  height="20%" roundedBottom="20px" >
+        <Flex direction='row' w='100%' height='50%'alignItems='center'  padding={8}  justifyContent='space-between'>
                 
         <Button leftIcon={<FaMoneyCheckAlt color="yellow"/>}/>
-                    <Text>Cashout</Text>
+                    <Text color='black'>Statistics</Text>
 
                 
                 <VStack spacing='5px'>
-                <IoIosNotificationsOutline/>
+                <FaDownload color="black"/>
 
                 </VStack>
                 
@@ -36,21 +38,23 @@ export default function StatisticPage() {
         </Box>
 
 
-<div className="h-3/4 w-full flex flex-col justify-center items-center">
+<div className="h-3/4 w-full  flex flex-col justify-center items-center">
 
 
   
        
-<div className="w-3/4 mr-5  h-16 rounded-2xl bg-gray-300 flex justify-center items-center text-black mt-10 mb-10 ">
-        <Flex w="100%" justifyContent='space-around' alignItems='center'>
-            <Button rounded={15} backgroundColor='white'><Text color='black'>Cards</Text></Button>
-            <Button><Text rounded={5} color='black'>Accounts</Text></Button>
-
-        </Flex>
+<div className="w-full   h-16   flex justify-center items-center text-black   ">
+        <HStack w="100%" justifyContent='space-around' alignItems='center'>
+            <Button rounded={10} backgroundColor='#438883' ><Text color='black'>Cards</Text></Button>
+            <Button rounded={15} ><Text color='black'>Cards</Text></Button>
+            <Button rounded={15} ><Text color='black'>Cards</Text></Button>
+            <Button rounded={15} ><Text color='black'>Cards</Text></Button>
+            
+        </HStack>
 
 
     </div>
-    <div className="w-3/4 mr-5  h-1/4 rounded-2xl bg-[#438883] bg-opacity-10 flex justify-center items-center text-black mt-10 mb-10 ">
+    <div className="w-full   h-3/4  bg-[#438883] bg-opacity-10 flex justify-center items-center text-black mt-10 mb-10 ">
         <Flex w="100%" justifyContent='space-around' alignItems='center'>
             <div className="bg-[#FFFFFF] rounded-full h-20 w-20 flex justify-center items-center"><RiBankFill color="green" className="w-20 h-10"/></div>
             <div className=" flex flex-col justify-center items-start ">
@@ -66,29 +70,47 @@ export default function StatisticPage() {
 
 
     </div>
-    <div className="w-3/4 mr-5  h-24 rounded-2xl bg-gray-300 flex justify-center items-center text-black mt-10 mb-10 ">
-    <Flex w="100%" justifyContent='space-around' alignItems='center' paddingRight={2}>
-            <div className=" rounded-full h-10 w-10 flex justify-center items-center"><SiCashapp  color="#888888" className="w-10 h-10"/></div>
-            <div className=" flex flex-col justify-center items-start ">
-                <Text fontSize='sm'>Withdraw to EOA</Text>
-                <Text fontSize='sm'>internet transfer </Text>
-                
-            </div>
+    <div className="w-full flex  justify-start items-start pl-10">
+        <Text color='black'>Top Spending</Text>
+
+    </div>
+    <div className="w-full p-4  h-full rounded-2xl  flex  text-black   ">
+    <VStack w="100%" h='100%'  justifyContent='space-around' alignItems='start' padding={1}>
             
+    <Flex direction='row' w='100%' height='20%'alignItems='start'   alignContent='center'  justifyContent='space-between'  mb="10px">
+                <HStack>
+                    <div className="flex justify-center items-center rounded-full h-10 w-10">
+                    <Button leftIcon={<MdLocalGroceryStore color="red"/>}/>
+                    </div>
+                    
+                    <Text color='black'>Utility</Text>
+                </HStack>             
+                
+                <Text color='red.500'>-$ 2,548.00</Text>
+            </Flex>
+            <Flex direction='row' w='100%' height='20%' alignItems='start'    justifyContent='space-between'  mb="10px">
+                <HStack>
+                    <Button leftIcon={<MdLocalGroceryStore color="red"/>}/>
+                    <Text color='black'>Transfer</Text>
+                </HStack>             
+                
+                <Text color='red.500'>-$ 2,548.00</Text>
+            </Flex>
+            <Flex direction='row' w='100%' height='20%'alignItems='start'    justifyContent='space-between'  mb="10px">
+                <HStack>
+                    <Button leftIcon={<MdLocalGroceryStore color="red"/>}/>
+                    <Text color='black'>Friends</Text>
+                </HStack>             
+                
+                <Text color='red.500'>-$ 2,548.00</Text>
+            </Flex>
           
 
-        </Flex>
+        </VStack>
 
 
     </div>
-    <div className="w-3/4 mr-5  h-16 rounded-2xl   border-green-400 border-2 flex justify-center items-center text-black mt-10 mb-10 ">
-        <Flex w="100%" justifyContent='space-around' alignItems='center'>
-            <Button color='green'>Next</Button>
-            
-        </Flex>
-
-
-    </div>
+    
     </div>
 
    
